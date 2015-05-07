3d90431d6642de902dcfe802a011b104d367f4b0
Amr <amr.e.dev@gmail.com>
2015-05-07 15:44:44 +0300

    building the logic for prob 3

--- sort3.m
+++ sort3.m
@@ -5,5 +5,18 @@
 % output: assend row vector
 
 function assend = sort3 (x1, x2, x3)
+   if (x1 > x2 && x1 > x3)
    
+   
+   elseif (x2 > x1 && x2 > x3)
+   
+   
+   elseif (x3 > x1 && x3 > x2)
+   
+   else
+       
+   end
+   
+   
+   assend = assend(end:-1:1);
 end
