f228611b0d222c6577b807868f4abe532a877f0c
Amr <amr.e.dev@gmail.com>
2015-05-07 13:52:31 +0300

    rewriting 3 to fix a bug

--- sort3.m
+++ sort3.m
@@ -5,33 +5,5 @@
 % output: assend row vector
 
 function assend = sort3 (x1, x2, x3)
-    if (x1 >= x2 && x1 >= x3)
-        assend(1) = x1;
-        if (x2 > x3)
-            assend(2) = x2;
-            assend(3) = x3;
-        else 
-            assend(2) = x3;
-            assend(3) = x2;
-        end
-    elseif (x1 >= x2 && x1 <= x3)
-        assend(1) = x2;
-        if (x3 > x1)
-            assend(2) = x3;
-            assend(3) = x1;
-        else 
-            assend(2) = x1;
-            assend(3) = x2;
-        end
-    else
-        assend(1) = x3;
-        if (x1 > x3)
-            assend(2) = x1;
-            assend(3) = x3;
-        else 
-            assend(2) = x3;
-            assend(3) = x1;
-        end
-    end
-    assend = assend(end:-1:1);
+   
 end
