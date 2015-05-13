0817f27dd4021b67d3644efc8039d1d60d9d5585
Amr <amr.e.dev@gmail.com>
2015-05-13 05:58:42 +0300

    Finished 3

--- sort3.m
+++ sort3.m
@@ -1,22 +1,43 @@
 % Introduction to Programming with MATLAB - MOOC
-% Homework 5 - Problem 2
+% Homework 5 - Problem 3
 
 % inputs: x1, x2, x3 scalers
 % output: assend row vector
 
 function assend = sort3 (x1, x2, x3)
-   if (x1 > x2 && x1 > x3)
-   
-   
-   elseif (x2 > x1 && x2 > x3)
-   
-   
-   elseif (x3 > x1 && x3 > x2)
-   
-   else
-       
-   end
-   
-   
-   assend = assend(end:-1:1);
+    assend = [];
+    if (x1 > x2)
+        if (x1 > x3)
+            if (x2 > x3)
+                assend(end+1) = x3;
+                assend(end+1) = x2;
+                assend(end+1) = x1;
+            else
+                assend(end+1) = x2;
+                assend(end+1) = x3;
+                assend(end+1) = x1;
+            end
+        else
+            assend(end+1) = x2;
+            assend(end+1) = x1;
+            assend(end+1) = x3;
+        end
+    else
+        if (x2 > x3)
+            if(x1 > x3)
+                assend(end+1) = x3;
+                assend(end+1) = x1;
+                assend(end+1) = x2;
+            else
+                assend(end+1) = x1;
+                assend(end+1) = x3;
+                assend(end+1) = x2;
+            end
+        else
+            assend(end+1) = x1;
+            assend(end+1) = x2;
+            assend(end+1) = x3;
+        end
+    end
+    
 end
