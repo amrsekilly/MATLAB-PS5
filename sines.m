f3d85a653c1dbceb7d85f5b9b92cb2ece36bad01
Amr <amr.e.dev@gmail.com>
2015-05-14 02:22:33 +0300

    organized sections

---
+++ sines.m
@@ -0,0 +1,28 @@
+% Introduction to Programming with MATLAB - MOOC
+% Homework 5 - Problem 7
+
+% pts, is an integer, 
+
+% but amp, f1, and f2 and are not necessarily integers
+
+% Output argument s1 is a row vector \
+% whose length (number of elements) equals pts.
+
+% s1 are the values of the sine contains f1 periods
+% s2 contains f2 periods
+
+% sums is the sum of s1 and s2
+
+% If f2 is omitted, then it should be set to a value that is 5% greater than f1.
+% If f1 is omitted also, then it should be set to 100
+% If amp is not provided, then it should default to 1
+% if pts is omitted as well, then it should be set to 1000.
+
+function [s1, s2, sums] = sines(pts,amp,f1,f2)
+    
+end
+
+
+
+
+
